5691e1196c55ff7c25ba5a0d7be0620f0e567b5e
harsh-bajani-transform <harsh.bajani@transformsolution.net>
2026-03-13 11:42:20 +0530

    feat: implement home page ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
+import BookCard from "@/components/BookCard";
+import HeroSection from "@/components/HeroSection";
+import { sampleBooks } from "@/lib/constants";
 import React from "react";
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold">Bookify</h1>
-    </div>
+    <main className="wrapper container">
+      <HeroSection />
+
+      <div className="library-books-grid">
+        {sampleBooks.map((book) => (
+          <BookCard
+            key={book._id}
+            title={book.title}
+            author={book.author}
+            coverURL={book.coverURL}
+            slug={book.slug}
+          />
+        ))}
+      </div>
+    </main>
   );
 };
 
