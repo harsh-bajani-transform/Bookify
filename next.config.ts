54ec3d4d68a21a4e2e4437b1327314a0f80d6bb0
harsh-bajani-transform <harsh.bajani@transformsolution.net>
2026-03-16 14:09:31 +0530

    feat: implement database setup and much more

--- next.config.ts
+++ next.config.ts
@@ -14,7 +14,7 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "covers.openlibrary.org" },
       {
         protocol: "https",
-        hostname: "lspfdyhgsrgsxcju.public.blob.vercel-storage.com",
+        hostname: "wrickcbmra3no6tf.public.blob.vercel-storage.com",
       },
     ],
   },
